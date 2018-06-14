93e7f19c3d0757e6d1abac00026ebcb7bcc53135
Harry Keartland <harrykeartland@gmail.com>
2018-06-14 12:56:52 +0100

    Added support for negative numbers, added cubic equations

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 scale = 5
 i=0
 function run(){
-	a = Math.floor((Math.random() * scale)+1);
-	b = Math.floor((Math.random() * scale)+1);
-	c = Math.floor((Math.random() * scale)+1);
-	d = Math.floor((Math.random() * scale)+1);
-
-	eq = a*c + "x² + " + (b*c + d*a) + "x + " + b*d
-	ans = "(" + a + "x + " + b + ")(" + c + "x + " + d + ")"
+	a = Math.floor((Math.random() * scale)+1) * (Math.random() < 0.5 ? -1 : 1);
+	b = Math.floor((Math.random() * scale)+1)* (Math.random() < 0.5 ? -1 : 1);
+	c = Math.floor((Math.random() * scale)+1)* (Math.random() < 0.5 ? -1 : 1);
+	d = Math.floor((Math.random() * scale)+1)* (Math.random() < 0.5 ? -1 : 1);
+	if (document.getElementById("checkBox").checked){
+		eq = "x³ + " + (a + b + c) + "x² + " + (a*b + a*c + b*c) + "x + " + a*b*c
+		ans = "(x + " + a + ")(x + " + b + ")(x + " + c + ")"
+	} else {
+		eq = a*c + "x² + " + (b*c + d*a) + "x + " + b*d
+		ans = "(" + a + "x + " + b + ")(" + c + "x + " + d + ")"
+	}
 	document.getElementById("container").innerHTML  += "<br><b>Q"+(i +1) + ".</b><div>"+ eq +"</div><button class='ans' id=\""+i+"b\" onclick=\"document.getElementById('"+i+"b').outerHTML ='<div>"+ans+"</div>'\">Ans</button>"
 	i++
 }
