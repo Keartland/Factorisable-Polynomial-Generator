6b18f7c14c218580e8402e868f1f2b300fcb4734
Harry Keartland <harrykeartland@gmail.com>
2018-06-16 00:16:45 +0100

    fixed commas

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
-scale = 5
 i=0
-function run(){
-	a = Math.floor((Math.random() * scale)+1) * (Math.random() < 0.5 ? -1 : 1);
-	b = Math.floor((Math.random() * scale)+1)* (Math.random() < 0.5 ? -1 : 1);
-	c = Math.floor((Math.random() * scale)+1)* (Math.random() < 0.5 ? -1 : 1);
-	d = Math.floor((Math.random() * scale)+1)* (Math.random() < 0.5 ? -1 : 1);
-	pmb = b < 0 ? "−" : "+"
-	if (document.getElementById("checkBox").checked){
-		eq = "x³ + " + (a + b + c) + "x² + " + (a*b + a*c + b*c) + "x + " + a*b*c
-		pma = a < 0 ? "−" : "+"
-		pmc = c < 0 ? "−" : "+"
-		ans = "(x " + pma + " " + Math.abs(a) + ")(x " + pmb + " " + Math.abs(b) + ")(x " + pmc + " " + Math.abs(c) + ")"
-	} else {
-		pmd = d < 0 ? "−" : "+"
-		eq = a*c + "x² + " + (b*c + d*a) + "x + " + b*d
-		ans = "(" + a + "x " + pmb + " " + Math.abs(b) + ")(" + c + "x " + pmd + " " + Math.abs(d) + ")"
+function run()
+{
+	terms=10
+	scale = 5;
+	a = Math.floor((Math.random() * scale) + 2);
+	b = Math.floor((Math.random() * scale) + 2);
+	c = Math.floor((Math.random() * scale) + 2);
+	r = Math.floor((Math.random() * scale) + 2);
+	eq = ""
+	g = gcd(gcd(a,b),c)
+	a /= g
+	b /= g
+	c /= g
+	for (j = 1; j <= terms; j++){
+		red = reduce((a * Math.pow(r, j)) + b,c)
+		fract = red[1] == 1 ? red[0] : red[0] + "/" + red[1]
+		eq = j != terms ? eq + fract + ", " : eq + fract
 	}
+	ans = "<br>Nth term expression: ("
+	ans = a == 1 ? ans + r + "ⁿ + " + b + ")" : ans + a + " * " + r + "ⁿ + " + b + ")"
+	ans = c == 1 ? ans.replace(/(\(|\))/g,"") : ans + " / " + c
 	document.getElementById("container").innerHTML  += "<br><b>Q"+(i +1) + ".</b><div>"+ eq +"</div><button class='ans' id=\""+i+"b\" onclick=\"document.getElementById('"+i+"b').outerHTML ='<div>"+ans+"</div>'\">Ans</button>"
 	i++
+	}
+
+function gcd(a,b){
+	return b ? gcd(b, a%b) : a;
+}
+
+
+function reduce(numerator,denominator){
+	numerator = Math.round(numerator)
+	denominator = Math.round(denominator)
+	g = gcd(numerator,denominator);
+  return [numerator/g, denominator/g];
 }
