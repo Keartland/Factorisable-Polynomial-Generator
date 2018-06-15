a0399c6fc805ab24675f23c823eddb007473f8d9
Harry Keartland <harrykeartland@gmail.com>
2018-06-15 23:16:18 +0100

    Updated title and changed support for negative numbers

--- main.js
+++ main.js
@@ -5,12 +5,16 @@ function run(){
 	b = Math.floor((Math.random() * scale)+1)* (Math.random() < 0.5 ? -1 : 1);
 	c = Math.floor((Math.random() * scale)+1)* (Math.random() < 0.5 ? -1 : 1);
 	d = Math.floor((Math.random() * scale)+1)* (Math.random() < 0.5 ? -1 : 1);
+	pmb = b < 0 ? "−" : "+"
 	if (document.getElementById("checkBox").checked){
 		eq = "x³ + " + (a + b + c) + "x² + " + (a*b + a*c + b*c) + "x + " + a*b*c
-		ans = "(x + " + a + ")(x + " + b + ")(x + " + c + ")"
+		pma = a < 0 ? "−" : "+"
+		pmc = c < 0 ? "−" : "+"
+		ans = "(x " + pma + " " + Math.abs(a) + ")(x " + pmb + " " + Math.abs(b) + ")(x " + pmc + " " + Math.abs(c) + ")"
 	} else {
+		pmd = d < 0 ? "−" : "+"
 		eq = a*c + "x² + " + (b*c + d*a) + "x + " + b*d
-		ans = "(" + a + "x + " + b + ")(" + c + "x + " + d + ")"
+		ans = "(" + a + "x " + pmb + " " + Math.abs(b) + ")(" + c + "x " + pmd + " " + Math.abs(d) + ")"
 	}
 	document.getElementById("container").innerHTML  += "<br><b>Q"+(i +1) + ".</b><div>"+ eq +"</div><button class='ans' id=\""+i+"b\" onclick=\"document.getElementById('"+i+"b').outerHTML ='<div>"+ans+"</div>'\">Ans</button>"
 	i++
