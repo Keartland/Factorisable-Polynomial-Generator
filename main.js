4ae6fca1ac04ef1968f8c94848b593b92fc59922
Harry Keartland <harrykeartland@gmail.com>
2018-06-30 10:48:08 +0100

    improved code

--- main.js
+++ main.js
@@ -1,28 +1,20 @@
 scale = 5
 i=0
 function run(){
-	a = Math.floor((Math.random() * scale)+1) * (Math.random() < 0.5 ? -1 : 1);
+	a = Math.floor((Math.random() * scale)+1)* (Math.random() < 0.5 ? -1 : 1);
 	b = Math.floor((Math.random() * scale)+1)* (Math.random() < 0.5 ? -1 : 1);
 	c = Math.floor((Math.random() * scale)+1)* (Math.random() < 0.5 ? -1 : 1);
-	pmb = b < 0 ? "−" : "+"
+	d = Math.floor((Math.random() * scale)+1)* (Math.random() < 0.5 ? -1 : 1);
 	if (document.getElementById("checkBox").checked){
-		pm1 = (a + b + c) < 0 ? "−" : "+"
-		pm2 = (a*b + a*c + b*c) < 0 ? "−" : "+"
-		pm3 = (a*b*c) < 0 ? "−" : "+"
-		eq = "x³ " + pm1 + " " + Math.abs(a + b + c) + "x² " + pm2 + " " + Math.abs(a*b + a*c + b*c) + "x " + pm3 + " " + Math.abs(a*b*c)
-		pma = a < 0 ? "−" : "+"
-		pmc = c < 0 ? "−" : "+"
-		ans = "(x " + pma + " " + Math.abs(a) + ")(x " + pmb + " " + Math.abs(b) + ")(x " + pmc	 + " " + Math.abs(c) + ")"
+		eq = "x³ " + pm(a + b + c) + " " + Math.abs(a + b + c) + "x² " + pm(a*b + a*c + b*c) + " " + Math.abs(a*b + a*c + b*c) + "x " + pm(a*b*c) + " " + Math.abs(a*b*c)
+		ans = "(x " + pm(a) + " " + Math.abs(a) + ")(x " + pm(b) + " " + Math.abs(b) + ")(x " + pm(c)	 + " " + Math.abs(c) + ")"
 	} else {
-		d = Math.floor((Math.random() * scale)+1)* (Math.random() < 0.5 ? -1 : 1);
-		pm1 = (b*c + d*a) < 0 ? "−" : "+"
-		pm2 = b*d < 0 ? "−" : "+"
-		eq = a*c + "x² " + pm1 + " " + Math.abs(b*c + d*a) + "x " + pm2 + " " + Math.abs(b*d)
-		pmd = d < 0 ? "−" : "+"
-		a = a != 1 ? a : ""
-		c = c != 1 ? c : ""
-		ans = "(" + a + "x " + pmb + " " + Math.abs(b) + ")(" + c + "x " + pmd + " " + Math.abs(d) + ")"
+		eq = a*c + "x² " + pm(b*c + d*a) + " " + Math.abs(b*c + d*a) + "x " + pm(b*d) + " " + Math.abs(b*d)
+		ans = "(" + a + "x " + pm(b) + " " + Math.abs(b) + ")(" + c + "x " + pm(d) + " " + Math.abs(d) + ")"
 	}
 	document.getElementById("container").innerHTML  += "<br><b>Q"+(i +1) + ".</b><div>"+ eq +"</div><button class='ans' id=\""+i+"b\" onclick=\"document.getElementById('"+i+"b').outerHTML ='<div>"+ans+"</div>'\">Ans</button>"
 	i++
+}
+function pm(value){
+	return value < 0 ? "−" : "+"
 }
