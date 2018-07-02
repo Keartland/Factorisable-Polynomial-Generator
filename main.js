d94f71729fe505e009830cd56f21c5a07081cf22
Harry Keartland <harrykeartland@gmail.com>
2018-07-02 18:30:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,12 +9,15 @@ function run(){
 		eq = "x³ " + pm(a + b + c) + "x² " + pm(a*b + a*c + b*c) + "x " + pm(a*b*c)
 		ans = "(x " + pm(a) + ")(x " + pm(b) + ")(x " + pm(c) + ")"
 	} else {
-		eq = a*c + "x² " + pm(b*c + d*a) + "x " + pm(b*d)
-		ans = "(" + a + "x " + pm(b) + ")(" + c + "x " + pm(d) + ")"
+		eq = xcoef(a*c) + "x² " + pm(b*c + d*a) + "x " + pm(b*d)
+		ans = "(" + xcoef(a) + "x " + pm(b) + ")(" + xcoef(c) + "x " + pm(d) + ")"
 	}
 	document.getElementById("container").innerHTML  += "<br><b>Q"+(i + 1) + ".</b><div>"+ eq +"</div><button class='ans' id=\""+i+"b\" onclick=\"document.getElementById('"+i+"b').outerHTML ='<div>"+ans+"</div>'\">Ans</button>"
 	i++
 }
 function pm(value){
 	return (value < 0 ? "−" : "+") + " " + Math.abs(value)
+}
+function xcoef(value){
+	return (value == 1 ? "" : value == -1 ? "-" : value)
 }
