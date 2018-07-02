9ad313f6e7446a48b06aedeb381b8abb9433740e
Harry Keartland <harrykeartland@gmail.com>
2018-07-02 18:05:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ function run(){
 		eq = "x³ " + pm(a + b + c) + " " + Math.abs(a + b + c) + "x² " + pm(a*b + a*c + b*c) + " " + Math.abs(a*b + a*c + b*c) + "x " + pm(a*b*c) + " " + Math.abs(a*b*c)
 		ans = "(x " + pm(a) + " " + Math.abs(a) + ")(x " + pm(b) + " " + Math.abs(b) + ")(x " + pm(c)	 + " " + Math.abs(c) + ")"
 	} else {
-		eq = a*c + "x² " + pm(b*c + d*a) + " " + Math.abs(b*c + d*a) + "x " + pm(b*d) + " " + Math.abs(b*d)
-		ans = "(" + a + "x " + pm(b) + " " + Math.abs(b) + ")(" + c + "x " + pm(d) + " " + Math.abs(d) + ")"
+		eq = pm(a*c) + Math.abs(a*c) + "x² " + pm(b*c + d*a) + " " + Math.abs(b*c + d*a) + "x " + pm(b*d) + " " + Math.abs(b*d)
+		ans = "(" + pm(a) + Math.abs(a) + "x " + pm(b) + " " + Math.abs(b) + ")(" + pm(c) + Math.abs(c) + "x " + pm(d) + " " + Math.abs(d) + ")"
 	}
 	document.getElementById("container").innerHTML  += "<br><b>Q"+(i +1) + ".</b><div>"+ eq +"</div><button class='ans' id=\""+i+"b\" onclick=\"document.getElementById('"+i+"b').outerHTML ='<div>"+ans+"</div>'\">Ans</button>"
 	i++
